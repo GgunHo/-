39f2a4d7c64a6ce626f68fd7d2f7ebb2614a78d5
GgunHo <abcd4276@naver.com>
2018-01-22 15:42:26 +0900

    Revert "1234"
    This reverts commit dd341d03bd985abf15c7fc502c5decb9a3aa3994.

--- checkPalindrome.py
+++
@@ -1,9 +0,0 @@
-def checkPalindrome(inputString):
-    if (inputString == inputString[::-1]):
-        return True
-        print("true")
-    else :
-        return False
-        print("false")
-        
-
--- shapeArea.py
+++
@@ -1,9 +0,0 @@
-def shapeArea(n):
-    sum = 1
-    
-    for i in range(n):
-        sum = 4*i + sum
-    
-    return sum
-    
-# 1 -> 1+ 4 -> 1+4+8 -> 1+4+8+12->....
